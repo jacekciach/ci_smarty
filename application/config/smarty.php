5491d27d75c8108c78f43a884e72e7d19b83b73a
Jacek Ciach <jacek.ciach@wp.eu>
2016-08-31 18:28:11 +0200

    Added example configurations in config/smarty.php

--- application/config/smarty.php
+++ application/config/smarty.php
@@ -1,5 +1,4 @@
 <?php
-
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 // required paths
@@ -8,11 +7,21 @@ $config['CompileDir']  = APPPATH.'Smarty/templates_c';
 $config['ConfigDir']   = APPPATH.'Smarty/configs';
 $config['TemplateDir'] = APPPATH.'templates';
 
-
+// example configuration for development
 $config['caching']          = Smarty::CACHING_OFF;
-//$config['cache_lifetime']   = 3600;
 $config['force_compile']    = FALSE;
 $config['compile_check']    = TRUE;
 $config['error_unassigned'] = TRUE;
 $config['error_reporting']  = E_ALL | E_STRICT;
 $config['debugging']        = FALSE;
+
+/*
+// example configuration for production
+$config['caching']          = Smarty::CACHING_LIFETIME_CURRENT;
+$config['cache_lifetime']   = 3600; // seconds
+$config['force_compile']    = FALSE;
+$config['compile_check']    = FALSE;
+$config['error_unassigned'] = FALSE;
+$config['error_reporting']  = null;
+$config['debugging']        = FALSE;
+*/
